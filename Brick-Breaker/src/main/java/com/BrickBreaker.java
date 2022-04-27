fa7cfafe83c4e5df607741a6b1c2f2b6b3b80178
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2022-04-27 01:59:25 -0700

    Commit refactored code for com dir

---
+++ Brick-Breaker/src/main/java/com/BrickBreaker.java
@@ -0,0 +1,37 @@
+package main.java.com;
+
+import javax.swing.*;
+import java.awt.EventQueue;
+import java.io.IOException;
+
+public class BrickBreaker extends JFrame {
+
+    public BrickBreaker() throws IOException {
+
+        initUI();
+    }
+
+    private void initUI() throws IOException {
+
+        add(new MenuScreen());
+        setTitle("Breakout");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            BrickBreaker game = null;
+            try {
+                game = new BrickBreaker();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            game.setVisible(true);
+        });
+    }
+}
