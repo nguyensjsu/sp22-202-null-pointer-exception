6a170808a8c19a75977491513693c4287737ba47
Anshul Kumar Shandilya <anshul.shandilya@sjsu.edu>
2022-05-03 17:28:03 -0700

    Created file to run on windows, and commented out Iterator design pattern files for now.

--- Brick-Breaker/src/main/java/Interfaces/IIterator.java
+++ Brick-Breaker/src/main/java/Interfaces/IIterator.java
@@ -1,8 +1,8 @@
-package main.java.Interfaces;
+// package main.java.Interfaces;
 
-public interface IIterator {
+// public interface IIterator {
 
-	public boolean hasNext();
-	public Object getNext();
+// 	public boolean hasNext();
+// 	public Object getNext();
 	
-}
+// }
