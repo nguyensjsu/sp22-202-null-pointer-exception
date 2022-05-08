9699119ab28fa411c73a751a32c0ae0163101f0c
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2022-05-07 20:11:54 -0700

    Add files via upload

--- Brick-Breaker/src/main/java/Objects/RacketDecorator.java
+++ Brick-Breaker/src/main/java/Objects/RacketDecorator.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 import java.awt.event.KeyEvent;
 import java.awt.Image;
 import java.awt.geom.Rectangle2D;
-import java.util.LinkedHashSet;
-import main.java.Config.*;
-import main.java.Interfaces.IKeyDirection;
-import main.java.com.NormalDirectionState;
-import main.java.com.SwitchedDirectionState;
 
 import main.java.Interfaces.*;
 
@@ -20,17 +15,9 @@ public class RacketDecorator implements IRacket{
     protected int imageWidth;
     protected int imageHeight;
     protected Image image;
-    private int dx;
-    private IKeyDirection directionState ;
-    NormalDirectionState normalDirectionState;
-    SwitchedDirectionState switchedDirectionState;
-    private LinkedHashSet<Integer> activeKeys = new LinkedHashSet<>();
     
     public RacketDecorator( IRacket R ) {
         racket = R ;
-        normalDirectionState = new NormalDirectionState();
-        switchedDirectionState = new SwitchedDirectionState();
-        directionState = normalDirectionState;
     }
 
     public void loadImage() throws IOException {
@@ -38,109 +25,62 @@ public class RacketDecorator implements IRacket{
     }
 
     public void move() {
-
-        setX(getX() + dx);
-        if (getX() <= 0) {
-            setX(0);
-        }
-
-        if (getX() >= Configurations.WIDTH - getImageWidth()) {
-            setX(Configurations.WIDTH - getImageWidth());
-        }
+        racket.move() ;
     }
 
     public void keyPressed(KeyEvent e, int select) {
-        activeKeys.add(e.getKeyCode());
-        directionState.specificMove(e.getKeyCode(), select, this);
+        racket.keyPressed(e, select);
     }
 
     public void keyReleased(KeyEvent e, int select) {
-        activeKeys.remove(e.getKeyCode());
-        int key = e.getKeyCode();
-        if (select == 0) {
-            if (key == KeyEvent.VK_LEFT) {
-                dx = 0;
-            }
-            if (key == KeyEvent.VK_RIGHT) {
-                dx = 0;
-            }
-        }
-        if (select == 1) {
-            if (key == KeyEvent.VK_A) {
-                dx = 0;
-            }
-            if (key == KeyEvent.VK_D) {
-                dx = 0;
-            }
-        }
-        if (!activeKeys.isEmpty()) {
-            directionState.specificMove((int)activeKeys.toArray()[activeKeys.size()-1], select, this);
-        }
+        racket.keyPressed(e, select);
     }
 
     public void setDirectionState( int val ) {
-        if ( val == 0 ) {
-            directionState = normalDirectionState ;
-        } else if ( val == 1 ) {
-            directionState = switchedDirectionState ;
-        }
+        racket.setDirectionState(val);
     }
 
     public void resetState() {
-       x = Configurations.INIT_PADDLE_X;
-       y = Configurations.INIT_PADDLE_Y;
+       racket.resetState();
     }
 
     public void changeDx(int dx) {
-        this.dx = dx;
+        racket.changeDx(dx);
     }
 
-
-    Rectangle2D rectangle2D = new Rectangle2D.Double();
-
     public void setX(double x) {
-
-        this.x = x;
+        racket.setX(x);
     }
 
     public double getX() {
-
-        return x;
+        return racket.getX();
     }
 
     public void setY(double y) {
-
-        this.y = y;
+        racket.setY(y);
     }
 
     public double getY() {
-
-        return y;
+        return racket.getY();
     }
 
     public int getImageWidth() {
-
-        return imageWidth;
+        return racket.getImageWidth();
     }
 
     public int getImageHeight() {
-
-        return imageHeight;
+        return racket.getImageHeight();
     }
 
     public Image getImage() {
-
-        return image;
+        return racket.getImage();
     }
 
     public Rectangle2D getRect() {
-        rectangle2D.setRect(x, y, image.getWidth(null), image.getHeight(null));
-        return rectangle2D;
+        return racket.getRect();  
     }
 
     public void getImageDimensions() {
-
-        imageWidth = image.getWidth(null);
-        imageHeight = image.getHeight(null);
+        racket.getImageDimensions();
     }
 }
